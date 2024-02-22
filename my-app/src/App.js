71c20d5702b1c914f45c932e3eb7e74b5f596661
PeetuVainio <peetu.vainio@gmail.com>
2024-02-22 12:03:48 +0200

    toimii

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -40,7 +40,7 @@ const handleFileUpload = async () => {
   setDriveLinks([]);
 
   // Lähetetään POST-pyyntö tiedoston lataamiseksi palvelimelle
-  const response = await fetch("https://localhost:5000/upload", {
+  const response = await fetch("http://localhost:5000/upload", {
       method: 'POST',
       body: formData
   });
